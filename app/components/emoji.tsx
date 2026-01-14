5bcc2a4ec577f26b492f10c54d54176fccf9daa5
ahui <8131746272@qq.com>
2026-01-14 15:48:39 +0800

    sky

--- app/components/emoji.tsx
+++ app/components/emoji.tsx
@@ -28,7 +28,7 @@ import NextImage from "next/image";
 export function CeoLogoIcon(props: { width?: number; height?: number; className?: string }) {
   return (
     <NextImage
-      src={SkyLogo}
+      src={CeoLogo}
       alt="logo"
       width={props.width ?? 30}
       height={props.height ?? 30}
